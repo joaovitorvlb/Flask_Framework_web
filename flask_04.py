56577a14708344b2ef94b4fcb65fcf4bb4e17dee
joaovitor <joaovitorvlb@hotmail.com>
2017-08-24 12:28:41 -0300

    Validate Routes +

--- flask_04.py
+++ flask_04.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def index():
     return 'Ola mundo'
 
+@app.route('/params/')
 @app.route('/params/<name>')
-def params(name):
-    return 'The parameter is : {}' .format(name) 
+@app.route('/params/<name>/<int:num>')
+def params(name = 'nama is default', num = 'nums is default'):
+    return 'The parameter is : {} {}' .format(name, num) 
 
 if __name__ == '__main__':
     app.run( debug=True, host = '192.168.1.118', port=8000)
